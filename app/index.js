0d0b91864ddf5ddd73597858ace777226b6d433e
Steven Collins <stec74@gmail.com>
2018-09-18 16:36:14 +0100

    initial layout for destinations overview

--- app/index.js
+++ app/index.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   $('.panel-collapse').on('show.bs.collapse', function () {
     $(this).siblings('.panel-heading').addClass('active');
   });
-  
+
   $('.panel-collapse').on('hide.bs.collapse', function () {
     $(this).siblings('.panel-heading').removeClass('active');
   });
