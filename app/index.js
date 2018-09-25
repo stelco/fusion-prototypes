6cc487c9f8c5772cb1aa33dfe39d33152327527a
Steven Collins <stec74@gmail.com>
2018-09-25 15:55:38 +0100

    mobile version updates

--- app/index.js
+++ app/index.js
@@ -12,10 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('jQuery works')
   }
 
-
   // initialize wow.js to watch when animations are scrolled into view
   new WOW().init();
-  
+
   /*collapse (accordian) chevron*/
   $('.panel-collapse').on('show.bs.collapse', function () {
     $(this).siblings('.panel-heading').addClass('active');
@@ -26,3 +25,21 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+/*read more links*/
+
+var dots = document.getElementById("dots");
+var moreText = document.getElementById("more");
+var btnText = document.getElementById("myBtn");
+
+if (dots.style.display === "none") {
+  dots.style.display = "inline";
+  btnText.innerHTML = "Read more";
+  moreText.style.display = "none";
+} else {
+  dots.style.display = "none";
+  btnText.innerHTML = "Read less";
+  moreText.style.display = "inline";
+}
+
+
